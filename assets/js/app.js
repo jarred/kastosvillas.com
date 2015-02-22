fa0353a83baa7e436e9c28b0c90e803865b693bd
Jarred Bishop <jarredbishop@gmail.com>
2015-02-22 10:48:53 +0900

    start on slideshow view

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,29 @@
+var KastosVillas = {};
 var $           = require('jquery');
 global.jQuery   = $;
 var _           = require('underscore');
 var Backbone    = require('backbone');
 Backbone.$ = $;
+
+KastosVillas.Views = {
+  Slideshow: require('./views/slideshow.js')
+};
+
+KastosVillas.App = {
+  init: function (){
+    this.extendViews();
+  },
+  extendViews: function (){
+    _.each($('.js-view'), function(el){
+      var $el = $(el);
+      var view = KastosVillas.Views[($el.data('view'))];
+      var v = new view({
+        el: el
+      });
+      $el.removeClass('js-view');
+    });
+  }
+}
+
+KastosVillas.App.init();
+module.exports = KastosVillas;
---
+++ assets/js/views/slideshow.js
@@ -0,0 +1,11 @@
+var _           = require('underscore');
+var Backbone    = require('backbone');
+var $           = require('jquery');
+
+var SlideshowView = Backbone.View.extend({
+  initialize: function(){
+    console.log('SlideshowView');
+  }
+});
+
+module.exports = SlideshowView;
